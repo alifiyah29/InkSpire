be9fac2b61975fa5a11084d58ee8b5ad00d938b3
Alifiyah Shahid <shahid.alifiyah@gmail.com>
2024-08-29 18:12:57 -0400

    functions as Props

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,24 +1,17 @@
-import React from "react";
-
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Ink-Spire</h1>
+      <h1>The Dojo Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a
-          href="/create"
-          style={{
-            color: "white",
-            backgroundColor: "#f1356d",
-            borderRadius: "8px",
-          }}
-        >
-          New Blog
-        </a>
+        <a href="/create" style={{ 
+          color: 'white', 
+          backgroundColor: '#f1356d',
+          borderRadius: '8px' 
+        }}>New Blog</a>
       </div>
     </nav>
   );
-};
-
+}
+ 
 export default Navbar;
