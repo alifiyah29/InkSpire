3bbebb45c40c2b1a1f2f3525b2293a91f1560caa
Alifiyah Shahid <shahid.alifiyah@gmail.com>
2024-08-30 10:01:51 -0400

    React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import Navbar from './Navbar';
-import Home from './Home';
+import Navbar from "./Navbar";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        {/* ALWAYS GONNA SHOW */}
+        <Navbar />
+        <div className="content">
+          <Routes>
+            {/* HOME PAGE */}
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
