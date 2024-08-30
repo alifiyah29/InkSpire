5208307d72434a1fd328510db9b1b6035d1a0a51
Alifiyah Shahid <shahid.alifiyah@gmail.com>
2024-08-30 10:46:36 -0400

    Route parameters

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./Navbar";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Create from "./Create";
+import BlogDetails from "./BlogDetails";
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
             <Route exact path="/" element={<Home />} />
             {/* CREATE */}
             <Route path="/create" element={<Create />} />
+            {/* BLOG DETAILS */}
+            <Route path="/blogs/:id" element={<BlogDetails />} />
           </Routes>
         </div>
       </div>
