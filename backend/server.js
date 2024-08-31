a7a4bda5b470976dcb3e3722d7f9ea0a75de7d9b
Alifiyah Shahid <shahid.alifiyah@gmail.com>
2024-08-30 23:45:52 -0400

    frontend and backend connected

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 const blogRoutes = require("./routes/blogRoutes.js");
 
 dotenv.config();
@@ -12,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//connection to MongoDB
+// Connection to MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
@@ -20,6 +21,14 @@ mongoose
 
 // Use the blog routes
 app.use("/blogs", blogRoutes);
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+// Handle any other requests and return the React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
 
 // Start the server
 app.listen(PORT, () => {
