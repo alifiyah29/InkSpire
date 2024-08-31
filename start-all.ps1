e681613ee1f69f359c45f755f06a3a63d0c8f167
Alifiyah Shahid <shahid.alifiyah@gmail.com>
2024-08-31 00:06:36 -0400

    Final Update

--- start-all.ps1
+++
@@ -1,8 +0,0 @@
-# Navigate to backend directory and start the backend server
-Start-Process -NoNewWindow -FilePath "npm" -ArgumentList "start" -WorkingDirectory "D:\Tech-Workspace\React.js\InkSpire\backend"
-
-# Wait for the backend to be up
-Start-Sleep -Seconds 5
-
-# Navigate to frontend directory and start the frontend server
-Start-Process -NoNewWindow -FilePath "npm" -ArgumentList "start" -WorkingDirectory "D:\Tech-Workspace\React.js\InkSpire\frontend"
